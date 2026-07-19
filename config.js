189419a6d7c171a0531f1dbbb15621cef9a8e65b
nanntai <desumikan826@gmail.com>
2026-07-20 01:06:18 +0900

    database13

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// config.js
 const supabaseUrl = "https://eqgyfkxiecozflnbypkl.supabase.co";
 const supabaseKey = "sb_publishable_3MQXaPuO9U3O_zub0LPoGg_N2pIYkIJ";
 
-// windowオブジェクトに持たせることで、他のファイルからも使えるようにします
+// 変数名「supabase」を使わずに、直接 window に入れる
 window.supabaseConnection = window.supabase.createClient(supabaseUrl, supabaseKey);
+
 console.log("Supabaseの初期化に成功しました");
